f0feae770cfc44a7047ac13cd0f7717324eed40d
akhand <akhand3108patel@gmail.com>
2023-09-22 16:14:48 +0530

    pass 1st stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	Uncomment this line to pass the first stage
 	"encoding/json"
 	"fmt"
 	"os"
@@ -37,8 +36,6 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 	}
 }
 
-
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -46,15 +43,15 @@ func main() {
 	command := os.Args[1]
 
 	if command == "decode" {
-		
+
 		bencodedValue := os.Args[2]
-		
+
 		decoded, err := decodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		
+
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
 	} else {
